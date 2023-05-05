a16994d1fc16c307c1d37459a900011ecb2e2919
Quentin Roulon <quevlahr@users.noreply.github.com>
2023-05-05 18:07:01 +0200

    return 404 for bad drug's name

--- app/drugs/[name]/page.tsx
+++ app/drugs/[name]/page.tsx
@@ -1,5 +1,26 @@
+import { notFound } from 'next/navigation';
+import { supabase } from '@/app/supabaseClient';
+
+const fetchDrug = async (name: string) => {
+  const supabaseResponse = await supabase
+    .from('drugs')
+    .select()
+    .ilike('name', name)
+    .limit(1)
+    .single();
+
+  return supabaseResponse.data;
+};
+
 type Props = { params: { name: string } };
 
-export default function Drug({ params }: Props) {
-  return <h1>{params.name}</h1>;
+export default async function Drug({ params: { name } }: Props) {
+  const drug = await fetchDrug(name);
+  if (drug === null) notFound();
+
+  return (
+    <h1>
+      {drug?.id} - {name}
+    </h1>
+  );
 }
