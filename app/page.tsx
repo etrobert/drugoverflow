76da8d934eb75b98a9706a8bce2c4d72f2ac3b81
erobert <etiennerobert33@gmail.com>
2023-05-06 12:54:31 +0200

    Move main page styling to RootLayout

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import List from './List';
 
 import type { Drug } from './types';
 
-import styles from './page.module.css';
-
 const fetchDrugsList = async () => {
   const { data } = await supabase.from('drugs').select();
   return data as Drug[]; // TODO: Infer type w/ Supabase CLI
@@ -16,12 +14,10 @@ export default async function Home() {
   const drugsList = await fetchDrugsList();
 
   return (
-    <main className={styles.main}>
-      <List>
-        {drugsList.map(({ name }) => (
-          <Link href={'drugs/' + name}>{name}</Link>
-        ))}
-      </List>
-    </main>
+    <List>
+      {drugsList.map(({ name }) => (
+        <Link href={'drugs/' + name}>{name}</Link>
+      ))}
+    </List>
   );
 }
