c5358f2bc0a5205e766a427d576ba3d0116533b1
erobert <etiennerobert33@gmail.com>
2023-05-06 12:18:48 +0200

    Remove unused variable from api route

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
+// To use request object use the following
+// export async function GET(request: Request) {
+export async function GET() {
   return new Response('Hello, Next.js!');
 }
