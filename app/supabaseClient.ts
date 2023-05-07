c6e7969d072177a97b5b50e8f7e3c8397344325f
erobert <etiennerobert33@gmail.com>
2023-05-07 16:15:12 +0200

    Move client creattion after testing the serviceRoleKey

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -5,12 +5,12 @@ const supabaseUrl = 'https://wovocqafhjqbsjrmvcpb.supabase.co';
 const anonKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvdm9jcWFmaGpxYnNqcm12Y3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjUwMzUsImV4cCI6MTk5ODgwMTAzNX0.XFlX9ciDuWMyDYa04TxDfjJTrkZpzkPa9C7MgnxtpgY';
 
-const client = createClient(supabaseUrl, anonKey);
-
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (serviceRoleKey === undefined)
   throw new Error('Missing SUPABASE_SERVICE_ROLE_KEY environment variable');
+
+const client = createClient(supabaseUrl, anonKey);
 
 const serverClient = createClient(supabaseUrl, serviceRoleKey);
 
