e352e23744328edb327b386366e078f601294c6b
erobert <etiennerobert33@gmail.com>
2023-05-07 15:36:14 +0200

    Enable insert of sames

--- app/api/same/route.ts
+++ app/api/same/route.ts
@@ -1,5 +1,13 @@
+import { serverClient as supabase } from '@/app/supabaseClient';
+
 // To use request object use the following
 // export async function GET(request: Request) {
 export async function POST() {
+  const { error } = await supabase
+    .from('sames')
+    .insert({ same: true, fact_id: 1 });
+
+  if (error) throw new Error(error.message);
+
   return new Response('Hello, Next.js!');
 }
--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const client = createClient(
+const client = createClient(
   'https://wovocqafhjqbsjrmvcpb.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvdm9jcWFmaGpxYnNqcm12Y3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjUwMzUsImV4cCI6MTk5ODgwMTAzNX0.XFlX9ciDuWMyDYa04TxDfjJTrkZpzkPa9C7MgnxtpgY'
 );
+
+const service_role_key = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (service_role_key === undefined)
+  throw new Error('Missing SUPABASE_SERVICE_ROLE_KEY environment variable');
+
+const serverClient = createClient(
+  'https://wovocqafhjqbsjrmvcpb.supabase.co',
+  service_role_key
+);
+
+export { client, serverClient };
