06c425e26e10c03e13a32ec09218f3386dd0de45
erobert <etiennerobert33@gmail.com>
2023-05-07 16:13:24 +0200

    Deduplicate supabaseUrl

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://wovocqafhjqbsjrmvcpb.supabase.co';
+
 const client = createClient(
-  'https://wovocqafhjqbsjrmvcpb.supabase.co',
+  supabaseUrl,
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvdm9jcWFmaGpxYnNqcm12Y3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjUwMzUsImV4cCI6MTk5ODgwMTAzNX0.XFlX9ciDuWMyDYa04TxDfjJTrkZpzkPa9C7MgnxtpgY'
 );
 
@@ -10,9 +12,6 @@ const service_role_key = process.env.SUPABASE_SERVICE_ROLE_KEY;
 if (service_role_key === undefined)
   throw new Error('Missing SUPABASE_SERVICE_ROLE_KEY environment variable');
 
-const serverClient = createClient(
-  'https://wovocqafhjqbsjrmvcpb.supabase.co',
-  service_role_key
-);
+const serverClient = createClient(supabaseUrl, service_role_key);
 
 export { client, serverClient };
