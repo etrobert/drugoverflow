4e8a81f458015555deb944fcb35178dca855102a
erobert <etiennerobert33@gmail.com>
2023-05-07 14:54:13 +0200

    Extract function called same

--- app/drugs/[name]/SameButton.tsx
+++ app/drugs/[name]/SameButton.tsx
@@ -1,13 +1,13 @@
 'use client';
 
+const same = async () => {
+  const response = await fetch('/api/same');
+  const text = await response.text();
+  console.log(text);
+};
+
 const SameButton = () => {
-  return (
-    <button
-      onClick={async () => console.log(await (await fetch('/api/same')).text())}
-    >
-      Same
-    </button>
-  );
+  return <button onClick={same}>Same</button>;
 };
 
 export default SameButton;
