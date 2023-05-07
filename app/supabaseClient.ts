b384374411d9a811aa38aad6dfcc582e1f92d3bb
erobert <etiennerobert33@gmail.com>
2023-05-07 16:14:34 +0200

    Extract anonKey in a variable

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -2,10 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wovocqafhjqbsjrmvcpb.supabase.co';
 
-const client = createClient(
-  supabaseUrl,
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvdm9jcWFmaGpxYnNqcm12Y3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjUwMzUsImV4cCI6MTk5ODgwMTAzNX0.XFlX9ciDuWMyDYa04TxDfjJTrkZpzkPa9C7MgnxtpgY'
-);
+const anonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvdm9jcWFmaGpxYnNqcm12Y3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjUwMzUsImV4cCI6MTk5ODgwMTAzNX0.XFlX9ciDuWMyDYa04TxDfjJTrkZpzkPa9C7MgnxtpgY';
+
+const client = createClient(supabaseUrl, anonKey);
 
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
