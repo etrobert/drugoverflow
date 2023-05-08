662a96c1c4d928a57d7dca413918727ea8ede767
erobert <etiennerobert33@gmail.com>
2023-05-08 17:55:04 +0200

    Add spaces in test

--- app/RadioGroup.test.tsx
+++ app/RadioGroup.test.tsx
@@ -47,6 +47,7 @@ describe('RadioGroup', () => {
 
   it('calls onChange with the correct value', async () => {
     const onChange = jest.fn();
+
     render(
       <RadioGroup
         values={['0', '1', '2']}
@@ -54,6 +55,7 @@ describe('RadioGroup', () => {
         defaultValue="0"
       />
     );
+
     await userEvent.click(screen.getByText('1'));
     expect(onChange).toHaveBeenCalledWith('1');
   });
