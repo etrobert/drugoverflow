2539bfc36af5d96d13d009588ac2e4041ef2aff8
erobert <etiennerobert33@gmail.com>
2023-05-08 13:58:26 +0200

    Add a test that tests the number of radio buttons

--- app/RadioGroup.test.tsx
+++ app/RadioGroup.test.tsx
@@ -4,6 +4,18 @@ import userEvent from '@testing-library/user-event';
 import RadioGroup from './RadioGroup';
 
 describe('RadioGroup', () => {
+  it('renders the correct number of radio buttons', () => {
+    render(
+      <RadioGroup
+        values={['0', '1', '2']}
+        onChange={() => {}}
+        defaultValue="0"
+      />
+    );
+
+    expect(screen.getAllByRole('radio')).toHaveLength(3);
+  });
+
   it('calls onChange with the correct value', async () => {
     const onChange = jest.fn();
     render(
