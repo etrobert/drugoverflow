0b8d98f44f303114f1ebd49c5bc59d2b8df23f4f
Quentin Roulon <quevlahr@users.noreply.github.com>
2023-05-15 15:53:13 +0200

    add addFact in form managment

--- app/drugs/[name]/AddFactForm.tsx
+++ app/drugs/[name]/AddFactForm.tsx
@@ -1,17 +1,30 @@
 import styles from './AddFactForm.module.css';
 
-const AddFactForm = () => (
-  <>
-    <form className={styles.form}>
-      <textarea
-        className={styles.textarea}
-        placeholder="Add your experience"
-      ></textarea>
-      <button className={styles.button} type="submit">
-        Add
-      </button>
-    </form>
-  </>
-);
+const AddFactForm = () => {
+  const addFact = async (data: { description: string }) => {
+    'use server';
+
+    console.log(data.description);
+  };
+
+  return (
+    <>
+      <form
+        className={styles.form}
+        // @ts-expect-error nextjs server action are not correctly typed
+        action={addFact}
+      >
+        <textarea
+          name="description"
+          className={styles.textarea}
+          placeholder="Add your experience"
+        ></textarea>
+        <button className={styles.button} type="submit">
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default AddFactForm;
