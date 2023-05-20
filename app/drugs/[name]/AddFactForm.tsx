353e516cf61c9f4d5fcd31906fca50325b064156
erobert <etiennerobert33@gmail.com>
2023-05-20 17:40:23 +0200

    Add server side validation

--- app/drugs/[name]/AddFactForm.tsx
+++ app/drugs/[name]/AddFactForm.tsx
@@ -5,13 +5,39 @@ type Props = {
   drugId: number;
 };
 
+const minLength = 5;
+const maxLength = 255;
+
+const validate = (data: FormData) => {
+  const description = data.get('description');
+
+  if (description === null) return { error: 'Description is required' };
+
+  if (typeof description !== 'string')
+    return { error: 'Description should be a string' };
+
+  if (description.length < minLength)
+    return { error: 'Description must be at least 5 characters long' };
+
+  if (description.length > maxLength)
+    return { error: 'Description must be at most 255 characters long' };
+
+  return { data: { description } };
+};
+
 const AddFactForm = ({ drugId }: Props) => {
-  const addFact = async (data: FormData) => {
+  const addFact = async (formData: FormData) => {
     'use server';
+
+    const { data, error: validationError } = validate(formData);
+
+    if (data === undefined) throw new Error(validationError);
+
+    const { description } = data;
 
     const { error } = await supabase
       .from('facts')
-      .insert({ description: data.get('description'), drug_id: drugId });
+      .insert({ description, drug_id: drugId });
 
     if (error) throw new Error(error.message);
   };
@@ -27,8 +53,8 @@ const AddFactForm = ({ drugId }: Props) => {
           className={styles['form-field']}
           placeholder="Add your experience"
           required
-          minLength={5}
-          maxLength={255}
+          minLength={minLength}
+          maxLength={maxLength}
         />
         <button className={styles['form-field']} type="submit">
           Add
