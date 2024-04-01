63fa981f945a2ab2669136f1bc033086ae3bf7be
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 18:48:30 +0200

    Fix landing page by using mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import Link from 'next/link';
 
-import { client as supabase } from './supabaseClient';
 import List from './List';
 
-import type { Drug } from './types';
-
-const fetchDrugsList = async () => {
-  const { data } = await supabase.from('drugs').select();
-  return data as Drug[]; // TODO: Infer type w/ Supabase CLI
-};
+const drugs = [
+  { id: 0, name: 'Paracetamol' },
+  { id: 1, name: 'Ibuprofen' },
+]
 
 export default async function Home() {
-  const drugsList = await fetchDrugsList();
-
   return (
     <>
       <h1>Drug Overflow</h1>
       <List>
-        {drugsList.map(({ id, name }) => (
+        {drugs.map(({ id, name }) => (
           <Link key={id} href={'drugs/' + name}>
             {name}
           </Link>
