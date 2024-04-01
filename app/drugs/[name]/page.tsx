ec8690bc6ccef648181eff091417f6d859b7cce8
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 18:52:57 +0200

    Fix drugs/[name]/page by using mock data

--- app/drugs/[name]/page.tsx
+++ app/drugs/[name]/page.tsx
@@ -1,48 +1,21 @@
-import { notFound } from 'next/navigation';
 import Link from 'next/link';
 
-import { client as supabase } from '@/app/supabaseClient';
 import List from '@/app/List';
 import SameButton from './SameButton';
 import AddFactForm from './AddFactForm';
 
 import type { Drug } from '@/app/types';
 
-export const revalidate = 0;
-
-const fetchDrug = async (name: string) => {
-  const supabaseResponse = await supabase
-    .from('drugs')
-    .select()
-    .ilike('name', name)
-    .limit(1)
-    .single();
-
-  return supabaseResponse.data as Drug | null;
-};
-
-const fetchFacts = async (drugId: number) => {
-  const supabaseResponse = await supabase
-    .from('facts')
-    .select()
-    .eq('drug_id', drugId);
-
-  return supabaseResponse.data;
-};
+const facts = [{ id: 0, description: 'I once had a seizure using this drug.' }];
+const drug = { id: 0, name: 'Paracetamol', created_at: '2021-01' };
 
 type Props = { params: { name: string } };
 
 export default async function Drug({ params: { name } }: Props) {
-  const drug = await fetchDrug(name);
-  if (drug === null) notFound();
-
-  const facts = await fetchFacts(drug.id);
-  if (facts === null) throw new Error('Could not fetch facts');
-
   return (
     <>
       <h1>
-        <Link href={'/'}>◁</Link> {drug.name}
+        <Link href={'/'}>◁</Link> {name}
       </h1>
       <main>
         <List>
