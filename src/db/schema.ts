b1cfbc0f162a14d59d69b7404646446d42099229
Etienne Robert <etiennerobert33@gmail.com>
2024-04-12 13:47:53 +0200

    Add facts table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, timestamp, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, timestamp, serial, text, integer } from 'drizzle-orm/pg-core';
 
 export const drugs = pgTable('drugs', {
   id: serial('id').primaryKey(),
@@ -7,3 +7,12 @@ export const drugs = pgTable('drugs', {
 });
 
 export type Drug = typeof drugs.$inferSelect;
+
+export const facts = pgTable('facts', {
+  id: serial('id').primaryKey(),
+  drugId: integer('drug_id')
+    .references(() => drugs.id)
+    .notNull(),
+  description: text('description').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
