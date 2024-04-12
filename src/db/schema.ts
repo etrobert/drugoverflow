9ea4b8113bdc822895286635a74694552844ff4f
Etienne Robert <etiennerobert33@gmail.com>
2024-04-12 12:30:49 +0200

    Configure drizzle-kit to generate

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, timestamp, serial, text } from 'drizzle-orm/pg-core'
+
+export const drugs = pgTable("drugs", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+  createdAt: timestamp("created_at").notNull().defaultNow()
+})
